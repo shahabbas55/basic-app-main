70e42e10d167b85cb5087b121180c0dc6f7a2ef6
shah abbas <abbas5555shah@gmail.com>
2024-10-09 11:10:49 +0500

    udate api

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,7 @@ app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
 
-// Catch all other requests and serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'D:/New folder/basic-app-main/build', 'index.html'));
-});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('https://basicapp-backend001.azurewebsites.net/api/message')
+    axios.get('http://localhost:5000/')
       .then(response => {
         setMessage(response.data.message);
       })
       .catch(error => {
-        console.error('There was an error!', error);
+        console.error('There was an error fetching the API!', error);
+        console.log('Error details:', error.response);
       });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div class="wrapper">
-          <h1>coming soon<span class="dot">.</span></h1>
+        <div className="wrapper">
+          <h1>{message || 'coming soon'}<span className="dot">.</span></h1>
         </div>
       </header>
     </div>
