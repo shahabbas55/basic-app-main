df7d4d7a2c317eec51567ba7c1e6dce188661519
shah abbas <abbas5555shah@gmail.com>
2024-10-09 11:58:01 +0500

    update test

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,14 @@ app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the backend test api!' });
 });
 
+// Serve React App
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/build'))); // Adjust 'client/build' to your actual build folder
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
