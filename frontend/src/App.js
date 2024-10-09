c9e80bb531840d179ce7a452f6a98c7a45b5adb2
shah abbas <abbas5555shah@gmail.com>
2024-10-09 16:37:01 +0500

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,22 @@ import './App.css';
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    axios.get('https://fftechnologies-eff0cjfuhndfczgr.canadacentral-01.azurewebsites.net/api/message')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the API!', error);
-        console.log('Error details:', error.response);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios.get('https://fftechnologies-eff0cjfuhndfczgr.canadacentral-01.azurewebsites.net/api/message')
+  //     .then(response => {
+  //       setMessage(response.data.message);
+  //     })
+  //     .catch(error => {
+  //       console.error('There was an error fetching the API!', error);
+  //       console.log('Error details:', error.response);
+  //     });
+  // }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="wrapper">
-          <h1>{message || 'coming soon'}<span className="dot">.</span></h1>
+          <h1>coming soon<span className="dot">.</span></h1>
         </div>
       </header>
     </div>
