e0715d19b9f8c6cbd93af5eac58ddd58818595c9
shah abbas <abbas5555shah@gmail.com>
2024-10-09 14:48:02 +0500

    new updated

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'FF Technologies Website' });
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
 
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the backend test api!' });
 });
 
-app.use(express.static("./frontend/build"));
-app.get("*",(req, res)=>{
-  res.sendFile(path.join(__dirname,"frontend","build","index.html"))
+// The catch-all handler for any request that doesn't match above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
