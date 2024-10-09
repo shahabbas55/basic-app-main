fb7b8e893c1f87878ff72c859c44fa94e4261ba2
shah abbas <abbas5555shah@gmail.com>
2024-10-09 11:15:12 +0500

    update change api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:5000/')
+    axios.get('http://localhost:5000/api/message')
       .then(response => {
         setMessage(response.data.message);
       })
